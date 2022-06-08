0b01eec3530a1cb827b3eb08f43c29c4d07f1b21
ezraa1 <ezrakipchirchir1034@gmail.com>
2022-06-08 15:18:21 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+// code your solution 
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}function mondayWork(work="go to the office"){
+    return `This Monday, I will ${work}.` 
+}
+function wrapAdjective(style) {
+    let result =(function(activity) {
+        return "You are " +style+ activity+
+        style + "!"
+    })
+    let emphatic = result;
+    return emphatic;   
+}
